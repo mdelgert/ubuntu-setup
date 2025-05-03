f74a6eb6bfa0b9ac6f2eeff82d3dc01c270bd321
Matthew Elgert <mdelgert@yahoo.com>
2025-05-03 19:50:20 -0400

    Add key management script and update setup.sh to include it

---
+++ keys.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script will copy keys to the .ssh directory and set the correct permissions
+
+# Define the source directory for keys
+KEYS_SOURCE_DIR="../ssh"
+
+# Check if the .ssh directory exists
+if [ ! -d "$HOME/.ssh" ]; then
+  mkdir -p "$HOME/.ssh"
+fi
+# Copy the keys to the .ssh directory
+cp -r "$KEYS_SOURCE_DIR"/* "$HOME/.ssh/"
+
+# Set the correct permissions
+chmod 700 "$HOME/.ssh"
+
+# Set the correct permissions for all files in the .ssh directory if they exist
+if [ "$(ls -A $HOME/.ssh)" ]; then
+  chmod 600 "$HOME/.ssh"/*
+fi
