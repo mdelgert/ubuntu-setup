7c429cf599d85a2d0e532313cc85662b03c02f23
Matthew Elgert <mdelgert@yahoo.com>
2025-05-08 00:08:20 -0400

    Add lidswitch.sh script to prevent suspend on lid close

---
+++ lidswitch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Make Ubuntu not suspend when laptop lid is closed
+# https://itsfoss.com/laptop-lid-suspend-ubuntu/
+# sudo nano /etc/systemd/logind.conf
+# HandleLidSwitch=ignore
+# HandleLidSwitchExternalPower=ignore
+# HandleLidSwitchDocked=ignore
