660b287cd7e0ca2e7e3aca4a82fecacbb1d8b3d1
Matthew Elgert <mdelgert@yahoo.com>
2025-05-19 22:38:49 -0400

    Remove commented-out VMware installer path in vmware-install.sh

--- vmware/vmware-install.sh
+++ vmware/vmware-install.sh
@@ -14,7 +14,6 @@ set -e
 
 # Define variables
 VMWARE_INSTALLER="/mnt/d1/apps/VMware-Workstation-Full-17.6.3-24583834.x86_64.bundle"
-#VMWARE_INSTALLER="/media/mdelgert/06e553b3-c8d2-475e-b98f-245551938440/downloads/VMware-Workstation-Full-17.6.0-24238078.x86_64.bundle"
 
 LOG_FILE="$HOME/vmware-installation.log"
 TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
