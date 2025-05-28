a7c8fcb98632eb78716a7fb8f93d147e1a4242a3
Matthew Elgert <mdelgert@yahoo.com>
2025-05-27 22:45:07 -0400

    Enable WSDD installation and uninstallation in Samba scripts

--- samba/samba-install.sh
+++ samba/samba-install.sh
@@ -6,13 +6,15 @@ set -e
 SAMBA_USER="mdelgert"
 
 sudo apt update
-sudo apt-get install samba -y
+sudo apt-get install samba wsdd-server -y
 sudo cp /etc/samba/smb.conf /etc/samba/smb.original
 sudo cp smb.conf.simple /etc/samba/smb.conf
 sudo systemctl restart smbd
 sudo systemctl enable smbd
+sudo systemctl restart wsdd-server
+sudo systemctl enable wsdd-server
 sudo samba --version
-# sudo smbpasswd -a $SAMBA_USER
-# ./wsdd-install.sh
+sudo wsdd --version
+sudo smbpasswd -a $SAMBA_USER
 
 exit 0
--- samba/samba-uninstall.sh
+++ samba/samba-uninstall.sh
@@ -3,9 +3,8 @@
 # Exit on any error
 set -e
 
-sudo apt purge samba samba-common samba-common-bin -y
+sudo apt purge samba samba-common samba-common-bin wsdd wsdd-server -y
 sudo apt autoremove --purge -y
 sudo rm -rf /etc/samba /var/log/samba /var/lib/samba
-# ./wsdd-uninstall.sh
 
 exit 0
